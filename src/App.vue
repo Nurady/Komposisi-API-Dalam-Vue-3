a7f69699a3dc41bfa2e6d9386971206690f75ef2
unknown <nursyafriady83@gmail.com>
2021-12-16 21:56:50 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>{{ name }}</div>
+    <div>{{ number }}</div>
 </template>
 
 <script>
@@ -9,13 +10,19 @@ import { ref } from 'vue'
 export default {
     setup() {
         const name = ref("Adis")
+        const number = ref(0)
 
         setTimeout(() => {
             name.value = "izzan"
+            
         }, 1000)
 
+        setInterval(() => {
+          number.value++
+        }, 1000);
+
         return {
-            name
+            name, number
         }
     }
 }
