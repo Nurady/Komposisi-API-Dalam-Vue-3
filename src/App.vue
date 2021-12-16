75c0cca3a725e13bce727c0839831228bf429687
unknown <nursyafriady83@gmail.com>
2021-12-16 22:56:23 +0800

    LifeCycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,45 @@
         {{ number }}
         <button @click="increment">+</button>
     </div>
+    <div>
+        <Posts />
+    </div>
+    <ul v-for="post in posts" :key="post.id">
+        <li>{{post.title}}</li>
+    </ul>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import useCounter from './helper/counter'
+import Posts from './components/Post.vue'
+import axios from 'axios'
 
 export default {
-   setup() {
-       const number = ref(0)
+    components: {
+        Posts
+    },
 
-       const { increment, decrement } = useCounter(number)
+    setup() {
+        const posts = ref([])
 
-       return {
-           number, increment, decrement
-       }
-   }
+        const getPosts = async () => {
+            let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            posts.value = data.slice(1,10)
+        }
+
+        onMounted(() => {
+            getPosts()
+        })
+
+        const number = ref(0)
+
+        const { increment, decrement } = useCounter(number)
+
+        return {
+            number, increment, decrement, posts
+        }
+    }
 }
 </script>
 
