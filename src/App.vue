efdbb9b031c99e056e92c3297a6e576cf733ef42
unknown <nursyafriady83@gmail.com>
2021-12-16 21:34:10 +0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ name }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    setup() {
+        const name = ref("Adis")
+
+        setTimeout(() => {
+            name.value = "izzan"
+        }, 1000)
+
+        return {
+            name
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
